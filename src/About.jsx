b001b89b215c92f9b745789d8736bbefd743357b
Javier Albertoni <javieralbertoni@gmail.com>
2023-10-25 17:03:54 -0300

    third commit

--- src/About.jsx
+++ src/About.jsx
@@ -18,13 +18,18 @@ function About() {
             </ Box>
             <Box sx={{
                 maxWidth: "70vw",
-                fontSize: "1.5vw",
+                fontSize: "1vw",
                 my: -10
             }}>
                 <h2>
                     Somos un grupo dedicado a crear páginas web para Argentina y Latinoamérica. Usamos las últimas tecnologías:
-                    Material, React, Firebase, Flutter, MongoDB, Next. Contáctanos!
+                    Material, React, Firebase, Flutter, MongoDB, Next.
+                    Esta página está hecha en React junto con Material, significa que es mas rápida que las páginas normales y se adapta
+                    a todas las pantallas.
                 </h2>
+                <Box sx={{display: "flex", alignItems: "center", justifyContent: "center", mt: 5}}>
+                <h1> Contáctanos! </h1>
+                </Box>
             </Box>
         </Box>)
 }
